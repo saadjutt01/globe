9eea22e697dcf5c37a36dd5c16f614dba85094b1
Saad Jutt <ihsan.distranger@gmail.com>
2021-09-02 23:00:02 +0500

    chore: prettier

--- utils/ExaggeratedElevationLayer.js
+++ utils/ExaggeratedElevationLayer.js
@@ -1,35 +1,33 @@
 define([
   "esri/layers/ElevationLayer",
-  "esri/layers/BaseElevationLayer"
-], function(ElevationLayer, BaseElevationLayer) {
-
+  "esri/layers/BaseElevationLayer",
+], function (ElevationLayer, BaseElevationLayer) {
   return BaseElevationLayer.createSubclass({
-
     properties: {
       exaggerationTopography: null,
-      exaggerationBathymetry: null
+      exaggerationBathymetry: null,
     },
 
-    load: function() {
+    load: function () {
       this._elevation = new ElevationLayer({
-        url: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/TopoBathy3D/ImageServer"
+        url: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/TopoBathy3D/ImageServer",
       });
       this.addResolvingPromise(this._elevation.load());
     },
 
-    fetchTile: function(level, row, col) {
+    fetchTile: function (level, row, col) {
       return this._elevation.fetchTile(level, row, col).then(
-        function(data) {
+        function (data) {
           for (let i = 0; i < data.values.length; i++) {
             if (data.values[i] >= 0) {
               data.values[i] = data.values[i] * this.exaggerationTopography;
-            }
-            else {
+            } else {
               data.values[i] = data.values[i] * this.exaggerationBathymetry;
             }
           }
           return data;
-        }.bind(this));
-    }
+        }.bind(this)
+      );
+    },
   });
 });
